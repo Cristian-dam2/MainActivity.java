e784ad60aab6100b2c81bf0a3d59dc842cbda981
Cristian-dam2 <114435303+Cristian-dam2@users.noreply.github.com>
2022-11-06 13:09:01 +0100

    domingo 13:08
    veamos

--- app/src/main/java/com/example/wheeloffortune/LoginActivity.java
+++ app/src/main/java/com/example/wheeloffortune/LoginActivity.java
@@ -2,19 +2,32 @@ package com.example.wheeloffortune;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private String nombre;
+
+    private EditText nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        nombre = (EditText) findViewById(R.id.login_campo_nombre);
+
+
     }
 
     public void iniciarJuego(View view) {
     }
-    
+
+    public void enviar(View view){
+        Intent myIntent = new Intent(this,MainActivity.class);
+        myIntent.putExtra("nombreusuario",nombre.getText().toString());
+        startActivity(myIntent);
+
+    }
+
 }
--- app/src/main/java/com/example/wheeloffortune/MainActivity.java
+++ app/src/main/java/com/example/wheeloffortune/MainActivity.java
@@ -2,16 +2,103 @@ package com.example.wheeloffortune;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] sectors = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",};
+    private static final int[] sectorDegress = new int[sectors.length];
+    private static final Random random = new Random();
+    private int degree = 0;
+    private boolean isSpinning = false;
+    private ImageView ruleta;
+    private Button botongirar;
+    private TextView score;
+    private Button cartelnombre;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ruleta = findViewById(R.id.ruleta);
+        botongirar = findViewById(R.id.botongirar);
+        score = (TextView) findViewById(R.id.puntos);
+        cartelnombre = findViewById(R.id.boton_nombre);
+        cartelnombre.setText(getIntent().getStringExtra("nombreusuario"));
+
+        getDegreeForSectors();
+        botongirar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isSpinning) {
+                    spin();
+                    isSpinning = true;
+                }
+            }
+        });
+
     }
 
 
+    private void spin() {
+        degree = random.nextInt(sectors.length - 1);
+        RotateAnimation rotateAnimacion = new RotateAnimation(0, (360 * sectors.length) + sectorDegress[degree],
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimacion.setDuration(4800);
+        rotateAnimacion.setFillAfter(true);
+        rotateAnimacion.setInterpolator(new DecelerateInterpolator());
+        rotateAnimacion.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Toast.makeText(MainActivity.this, "MUCHA SUERTE", Toast.LENGTH_SHORT).show();
+                botongirar.setEnabled(false);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                int valor = Integer.valueOf(sectors[sectors.length - (degree + 1)]);
+                Toast.makeText(MainActivity.this, "Tienes " + valor + " puntos.", Toast.LENGTH_LONG).show();
+                sumarPuntos(valor);
+                isSpinning = false;
+                botongirar.setEnabled(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        ruleta.startAnimation(rotateAnimacion);
+    }
+
+
+    private void getDegreeForSectors() {
+        int sectorDegree = 360 / sectors.length;
+        for (int i = 0; i < sectors.length; i++) {
+            sectorDegress[i] = (i + 1) * sectorDegree;
+        }
+
+
+    }
+
+    private void sumarPuntos(int valornuevo) {
+        String valorviejo = score.getText().toString();
+        int numeroviejo = Integer.valueOf(valorviejo);
+        int suma = valornuevo + numeroviejo;
+        score.setText(String.valueOf(suma));
+    }
 }
