89d5579b062e3d3d2eb34d16107a609b91c10653
David-625-Perez <114435372+David-625-Perez@users.noreply.github.com>
2023-02-04 18:18:06 +0100

    Javadocs

--- app/src/main/java/com/example/wheeloffortune/Auxiliares/C.java
+++ app/src/main/java/com/example/wheeloffortune/Auxiliares/C.java
@@ -1,17 +1,20 @@
 package com.example.wheeloffortune.Auxiliares;
 
 public class C  {
-    private boolean myBool;
-    public void setBool(boolean b) {
-        myBool = b;
-        if (mOnChange != null) mOnChange.onBoolChange(b);
-    }
-
     public BoolChangeListener mOnChange = null;
     public interface BoolChangeListener {
         void onBoolChange(boolean b);
     }
 
+    /**
+     * Cambia el boolean guardado e intenta ejecutar onBoolChange(boolean)
+     * @param b
+     */
+    public void setBool(boolean b) {
+        if (mOnChange != null) {
+            mOnChange.onBoolChange(b);
+        }
+    }
     public void setOnBoolChangeListener(BoolChangeListener bcl) {
         mOnChange = bcl;
     }
